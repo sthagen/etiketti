1cdcfc2b245d3197b61fb6ab38e8d05ee7fdb944
Stefan Hagen <stefan@hagen.link>
2023-01-22 10:59:12 +0100

    Happy linter, happy winter

--- etiketti/__init__.py
+++ etiketti/__init__.py
@@ -4,4 +4,3 @@ __version__ = '2023.1.22+parent.1c9b943f'
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
-
