e6349d921e80bb65d96ff7b47edc04ecc3bfb657
Stefan Hagen <stefan@hagen.link>
2023-01-22 18:36:01 +0100

    Bumped impl version for release 2023.1.22

--- etiketti/__init__.py
+++ etiketti/__init__.py
@@ -6,8 +6,8 @@ import pathlib
 from typing import List, no_type_check
 
 # [[[fill git_describe()]]]
-__version__ = '2023.1.22+parent.1c9b943f'
-# [[[end]]] (checksum: 3bd1931402285ec7d6d496ce3b055c6f)
+__version__ = '2023.1.22+parent.d22707ee'
+# [[[end]]] (checksum: 8a9143f24a73aea9df32917d25afb8ec)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
