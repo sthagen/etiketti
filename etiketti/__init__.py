ac730d1081918c35e526aa792b8227529642eb22
Stefan Hagen <stefan@hagen.link>
2023-01-22 10:49:22 +0100

    Finalized the CPSR coding of the version hook

--- etiketti/__init__.py
+++ etiketti/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2023.1.21+parent.85ecfd90'
-# [[[end]]] (checksum: 9fb8b8c0eb390d5ee5ac54b6fb82cf2b)
+__version__ = '2023.1.22+parent.1c9b943f'
+# [[[end]]] (checksum: 3bd1931402285ec7d6d496ce3b055c6f)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
