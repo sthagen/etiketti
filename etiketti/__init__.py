629c839d2ab20a9cae11acecc77195e602c1533f
Stefan Hagen <stefan@hagen.link>
2023-07-01 16:59:01 +0200

    Derived some app specific variable values from module (well file) docstring and the place

--- etiketti/__init__.py
+++ etiketti/__init__.py
@@ -12,17 +12,18 @@ __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
 
-APP_NAME = 'Label (Finnish: etiketti) some files.'
-APP_ALIAS = 'etiketti'
-APP_ENV = 'ETIKETTI'
-APP_VERSION = __version__
+APP_ALIAS = str(pathlib.Path(__file__).parent.name)
+APP_ENV = APP_ALIAS.upper()
+APP_NAME = locals()['__doc__']
 DEBUG = bool(os.getenv(f'{APP_ENV}_DEBUG', ''))
 VERBOSE = bool(os.getenv(f'{APP_ENV}_VERBOSE', ''))
 QUIET = False
 STRICT = bool(os.getenv(f'{APP_ENV}_STRICT', ''))
 ENCODING = 'utf-8'
 ENCODING_ERRORS_POLICY = 'ignore'
-DEFAULT_CONFIG_NAME = '.etiketti.json'
+DEFAULT_CONFIG_NAME = f'.{APP_ALIAS}.json'
+
+APP_VERSION = __version__
 DEFAULT_LF_ONLY = 'YES'
 log = logging.getLogger()  # Module level logger is sufficient
 LOG_FOLDER = pathlib.Path('logs')
